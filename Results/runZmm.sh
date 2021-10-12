b8512b13369bfc3459db7126b624269620b2e10a
Yongbin Feng <ybfeng94@gmail.com>
2021-10-12 20:16:56 +0200

    remove un-used directories

--- Results/runZmm.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-LUMI=2318.3
-
-root -l -q  plotZmm.C+\(\"Zmm\",${LUMI}\)
-root -l -q  plotZmmSystematics.C+\(\"Zmm\",${LUMI}\)
-root -l -q  plotZmmCorrelations.C+\(\"Zmm\",${LUMI}\)
-root -l -q  plotZmmStatCorrelations.C+\(\"Zmm\",${LUMI}\)
