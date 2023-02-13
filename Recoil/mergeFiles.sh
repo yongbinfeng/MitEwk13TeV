0e98a93c17e86a0e5203f2f938745311f77dbf5c
Yongbin Feng <ybfeng94@gmail.com>
2023-02-13 17:43:26 +0100

    update mergefiles script

--- Recoil/mergeFiles.sh
+++ Recoil/mergeFiles.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 # merge root files based on filenames
 
-INPUTDIR=/eos/uscms/store/user/yofeng/Ntuples_LowPU/13TeV/Selections/Zmumu_pT20
-OUTPUTDIR=$INPUTDIR/merged
+doZmm=True
+doWm=True
 
-for i in `ls $INPUTDIR | grep root | awk -F'_' '{print $1}' | sort | uniq`
-do
-    echo $i
-    echo "hadd $OUTPUTDIR/$i.root $INPUTDIR/$i*"
-    hadd $OUTPUTDIR/$i.root $INPUTDIR/$i*
-done
+if [ $doZmm = True ]; then
+    INPUTDIR=/eos/uscms/store/user/yofeng/Ntuples_LowPU/13TeV/Selections/Zmumu_pT20
+    OUTPUTDIR=$INPUTDIR/merged
+
+    if [ ! -d $OUTPUTDIR ]; then
+        echo "Creating output directory $OUTPUTDIR"
+        mkdir $OUTPUTDIR
+    fi
+
+    for i in $(ls $INPUTDIR | grep root | awk -F'_' '{print $1}' | sort | uniq); do
+        echo $i
+        echo "hadd $OUTPUTDIR/$i.root $INPUTDIR/$i*root"
+        hadd $OUTPUTDIR/$i.root $INPUTDIR/$i*root
+    done
+fi
+
+if [ $doWm = True ]; then
+    INPUTDIR=/eos/uscms/store/user/yofeng/Ntuples_LowPU/13TeV/Selections/Wmunu
+    OUTPUTDIR=$INPUTDIR/merged
+
+    if [ ! -d $OUTPUTDIR ]; then
+        echo "Creating output directory $OUTPUTDIR"
+        mkdir $OUTPUTDIR
+    fi
+
+    for i in $(ls $INPUTDIR | grep "wm\|data" | grep root | awk -F'_' '{print $1}' | sort | uniq); do
+        echo $i
+        echo "hadd $OUTPUTDIR/$i.root $INPUTDIR/$i*root"
+        #hadd $OUTPUTDIR/$i.root $INPUTDIR/$i*root
+    done
+fi
