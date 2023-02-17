34fa676d456e301b8afe1bb77738df9569d759ae
Yongbin Feng <ybfeng94@gmail.com>
2023-02-17 06:16:53 +0100

    update output dir for recoil calibrations

--- Recoil/runRecoilFits13TeV.sh
+++ Recoil/runRecoilFits13TeV.sh
@@ -10,7 +10,7 @@ root -l -b -q fitRecoilWl.C+\(\"${In13_Wm}\",2,2,1,-1,0,\"met\",\"metPhi\",\"met
 # 13 TeV Z MC
 root -l -b -q fitRecoilZll.C+\(\"${In13_Zmm}\",\"zmm.root\",2,2,1,\"met\",\"metPhi\",\"met\",\"${OUT}/ZmmMC_PF_13TeV_2G\",${LUMI13},0,0\)
 # 13 TeV Z Data
-root -l -b -q fitRecoilZll.C+\(\"${In13_Zmm}\",\"data.root\",2,2,0,\"met\",\"metPhi\",\"met\",\"${OUT}/ZmmData_PF_13TeV_2G_bkg_fixRoch\",${LUMI13},0,0\)
+root -l -b -q fitRecoilZll.C+\(\"${In13_Zmm}\",\"data.root\",2,2,0,\"met\",\"metPhi\",\"met\",\"${OUT}/ZmmData_PF_13TeV_2G\",${LUMI13},0,0\)
 
 #-----------------------------------------------
 # electron
@@ -21,16 +21,16 @@ root -l -b -q fitRecoilWl.C+\(\"${In13_We}\",2,2,1,-1,0,\"met\",\"metPhi\",\"met
 # 13 TeV Z MC
 root -l -b -q fitRecoilZll.C+\(\"${In13_Zee}\",\"zee.root\",2,2,1,\"met\",\"metPhi\",\"met\",\"${OUT}/ZeeMC_PF_13TeV_2G\",${LUMI13},0,0,0,1\)
 # 13 TeV Z Data
-root -l -b -q fitRecoilZll.C+\(\"${In13_Zee}\",\"data.root\",2,2,0,\"met\",\"metPhi\",\"met\",\"${OUT}/ZeeData_PF_13TeV_2G_bkg_fixRoch\",${LUMI13},0,0,0,1\)
+root -l -b -q fitRecoilZll.C+\(\"${In13_Zee}\",\"data.root\",2,2,0,\"met\",\"metPhi\",\"met\",\"${OUT}/ZeeData_PF_13TeV_2G\",${LUMI13},0,0,0,1\)
 
 ###################################################################################################
 #--------------------------------------------------------------------------------------------------
 #-            ETA-BINNED
 # -------------------------------------------------------------------------------------------------
 # central region
-root -l -b -q fitRecoilWl.C+\(\"${In13_Wm}\",2,2,1,1,0,\"met\",\"metPhi\",\"met\",\"${OUT}/WmpMC_PF_13TeV_Eta$1\",${LUMI13},1,0\)
+root -l -b -q fitRecoilWl.C+\(\"${In13_Wm}\",2,2,1,1,0,\"met\",\"metPhi\",\"met\",\"${OUT}/WmpMC_PF_13TeV_Eta1\",${LUMI13},1,0\)
 root -l -b -q fitRecoilWl.C+\(\"${In13_Wm}\",2,2,1,-1,0,\"met\",\"metPhi\",\"met\",\"${OUT}/WmmMC_PF_13TeV_Eta1\",${LUMI13},1,0\)
-root -l -b -q fitRecoilZll.C+\(\"${In13_Zmm}\",\"zmm.root\",2,2,1,\"met\",\"metPhi\",\"met\",\"${OUT}/ZmmMC_PF_13TeV_Eta$1\",${LUMI13},1,0\)
+root -l -b -q fitRecoilZll.C+\(\"${In13_Zmm}\",\"zmm.root\",2,2,1,\"met\",\"metPhi\",\"met\",\"${OUT}/ZmmMC_PF_13TeV_Eta1\",${LUMI13},1,0\)
 root -l -b -q fitRecoilZll.C+\(\"${In13_Zmm}\",\"data.root\",2,2,0,\"met\",\"metPhi\",\"met\",\"${OUT}/ZmmData_PF_13TeV_Eta1\",${LUMI13},1,0\)
 # mid region
 root -l -b -q fitRecoilWl.C+\(\"${In13_Wm}\",2,2,1,1,0,\"met\",\"metPhi\",\"met\",\"${OUT}/WmpMC_PF_13TeV_Eta2\",${LUMI13},2,0\)
@@ -40,7 +40,7 @@ root -l -b -q fitRecoilZll.C+\(\"${In13_Zmm}\",\"data.root\",2,2,0,\"met\",\"met
 # forward region
 root -l -b -q fitRecoilWl.C+\(\"${In13_Wm}\",2,2,1,1,0,\"met\",\"metPhi\",\"met\",\"${OUT}/WmpMC_PF_13TeV_Eta3\",${LUMI13},3,0\)
 root -l -b -q fitRecoilWl.C+\(\"${In13_Wm}\",2,2,1,-1,0,\"met\",\"metPhi\",\"met\",\"${OUT}/WmmMC_PF_13TeV_Eta3\",${LUMI13},3,0\)
-root -l -b -q fitRecoilZll.C+\(\"${In13_Zmm}\",\"zmm.root\",2,2,1,\"met\",\"metPhi\",\"met\",\"${OUT}/ZmmMC_PF_13TeV_Eta$3\",${LUMI13},3,0\)
+root -l -b -q fitRecoilZll.C+\(\"${In13_Zmm}\",\"zmm.root\",2,2,1,\"met\",\"metPhi\",\"met\",\"${OUT}/ZmmMC_PF_13TeV_Eta3\",${LUMI13},3,0\)
 root -l -b -q fitRecoilZll.C+\(\"${In13_Zmm}\",\"data.root\",2,2,0,\"met\",\"metPhi\",\"met\",\"${OUT}/ZmmData_PF_13TeV_Eta3\",${LUMI13},3,0\)
 
 #----------------------------------------
--- Recoil/runRecoilFits5TeV.sh
+++ Recoil/runRecoilFits5TeV.sh
@@ -10,7 +10,7 @@ root -l -b -q fitRecoilWl.C+\(\"${In5_Wm}\",2,2,1,-1,0,\"met\",\"metPhi\",\"met\
 # 5 TeV Z MC
 root -l -b -q fitRecoilZll.C+\(\"${In5_Zmm}\",\"zmm.root\",2,2,1,\"met\",\"metPhi\",\"met\",\"${OUT}/ZmmMC_PF_5TeV_2G\",${LUMI5},0,0,1,0\)
 # 5 TeV Z Data
-root -l -b -q fitRecoilZll.C+\(\"${In5_Zmm}\",\"data.root\",2,2,0,\"met\",\"metPhi\",\"met\",\"${OUT}/ZmmData_PF_5TeV_2G_bkg_fixRoch\",${LUMI5},0,0,1,0\)
+root -l -b -q fitRecoilZll.C+\(\"${In5_Zmm}\",\"data.root\",2,2,0,\"met\",\"metPhi\",\"met\",\"${OUT}/ZmmData_PF_5TeV_2G\",${LUMI5},0,0,1,0\)
 
 #-----------------------------------------------
 # electron
@@ -21,16 +21,16 @@ root -l -b -q fitRecoilWl.C+\(\"${In5_We}\",2,2,1,-1,0,\"met\",\"metPhi\",\"met\
 # 5 TeV Z MC
 root -l -b -q fitRecoilZll.C+\(\"${In5_Zee}\",\"zee.root\",2,2,1,\"met\",\"metPhi\",\"met\",\"${OUT}/ZeeMC_PF_5TeV_2G\",${LUMI5},0,0,1,1\)
 # 5 TeV Z Data
-root -l -b -q fitRecoilZll.C+\(\"${In5_Zee}\",\"data.root\",2,2,0,\"met\",\"metPhi\",\"met\",\"${OUT}/ZeeData_PF_5TeV_2G_bkg_fixRoch\",${LUMI5},0,0,1,1\)
+root -l -b -q fitRecoilZll.C+\(\"${In5_Zee}\",\"data.root\",2,2,0,\"met\",\"metPhi\",\"met\",\"${OUT}/ZeeData_PF_5TeV_2G\",${LUMI5},0,0,1,1\)
 
 ###################################################################################################
 #--------------------------------------------------------------------------------------------------
 #-            ETA-BINNED
 # -------------------------------------------------------------------------------------------------
 # central region
-root -l -b -q fitRecoilWl.C+\(\"${In5_Wm}\",2,2,1,1,0,\"met\",\"metPhi\",\"met\",\"${OUT}/WmpMC_PF_5TeV_Eta$1\",${LUMI5},1,0,1,0\)
+root -l -b -q fitRecoilWl.C+\(\"${In5_Wm}\",2,2,1,1,0,\"met\",\"metPhi\",\"met\",\"${OUT}/WmpMC_PF_5TeV_Eta1\",${LUMI5},1,0,1,0\)
 root -l -b -q fitRecoilWl.C+\(\"${In5_Wm}\",2,2,1,-1,0,\"met\",\"metPhi\",\"met\",\"${OUT}/WmmMC_PF_5TeV_Eta1\",${LUMI5},1,0,1,0\)
-root -l -b -q fitRecoilZll.C+\(\"${In5_Zmm}\",\"zmm.root\",2,2,1,\"met\",\"metPhi\",\"met\",\"${OUT}/ZmmMC_PF_5TeV_Eta$1\",${LUMI5},1,0,1,0\)
+root -l -b -q fitRecoilZll.C+\(\"${In5_Zmm}\",\"zmm.root\",2,2,1,\"met\",\"metPhi\",\"met\",\"${OUT}/ZmmMC_PF_5TeV_Eta1\",${LUMI5},1,0,1,0\)
 root -l -b -q fitRecoilZll.C+\(\"${In5_Zmm}\",\"data.root\",2,2,0,\"met\",\"metPhi\",\"met\",\"${OUT}/ZmmData_PF_5TeV_Eta1\",${LUMI5},1,0,1,0\)
 # mid region
 root -l -b -q fitRecoilWl.C+\(\"${In5_Wm}\",2,2,1,1,0,\"met\",\"metPhi\",\"met\",\"${OUT}/WmpMC_PF_5TeV_Eta2\",${LUMI5},2,0,1,0\)
@@ -40,7 +40,7 @@ root -l -b -q fitRecoilZll.C+\(\"${In5_Zmm}\",\"data.root\",2,2,0,\"met\",\"metP
 # forward region
 root -l -b -q fitRecoilWl.C+\(\"${In5_Wm}\",2,2,1,1,0,\"met\",\"metPhi\",\"met\",\"${OUT}/WmpMC_PF_5TeV_Eta3\",${LUMI5},3,0,1,0\)
 root -l -b -q fitRecoilWl.C+\(\"${In5_Wm}\",2,2,1,-1,0,\"met\",\"metPhi\",\"met\",\"${OUT}/WmmMC_PF_5TeV_Eta3\",${LUMI5},3,0,1,0\)
-root -l -b -q fitRecoilZll.C+\(\"${In5_Zmm}\",\"zmm.root\",2,2,1,\"met\",\"metPhi\",\"met\",\"${OUT}/ZmmMC_PF_5TeV_Eta$3\",${LUMI5},3,0,1,0\)
+root -l -b -q fitRecoilZll.C+\(\"${In5_Zmm}\",\"zmm.root\",2,2,1,\"met\",\"metPhi\",\"met\",\"${OUT}/ZmmMC_PF_5TeV_Eta3\",${LUMI5},3,0,1,0\)
 root -l -b -q fitRecoilZll.C+\(\"${In5_Zmm}\",\"data.root\",2,2,0,\"met\",\"metPhi\",\"met\",\"${OUT}/ZmmData_PF_5TeV_Eta3\",${LUMI5},3,0,1,0\)
 
 #----------------------------------------
